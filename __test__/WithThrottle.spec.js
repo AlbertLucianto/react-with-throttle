2e85600fbeec525bf9de05fac868cdbe80292cee
AlbertLucianto <albertlucianto@gmail.com>
2019-03-07 00:24:27 +0800

    [Testing] setup and add test cases

---
+++ __test__/WithThrottle.spec.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// eslint-disable-next-line import/no-unresolved
+import WithThrottle from 'react-with-throttle';
+import { render, cleanup } from 'react-testing-library';
+
+afterEach(cleanup);
+
+const waitFor = async ms => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
+
+class TickingCounter extends Component {
+  state = {
+    count: 0,
+  };
+
+  interval = null;
+
+  componentDidMount() {
+    const { interval } = this.props;
+
+    this.interval = setInterval(() => {
+      this.setState(({ count }) => ({ count: count + 1 }));
+    }, interval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const {
+      wait,
+      options,
+      render: renderContent,
+    } = this.props;
+
+    const { count } = this.state;
+
+    return (
+      <WithThrottle
+        value={count}
+        wait={wait}
+        options={options}
+      >
+        {renderContent}
+      </WithThrottle>
+    );
+  }
+}
+
+TickingCounter.propTypes = {
+  wait: PropTypes.number.isRequired,
+  interval: PropTypes.number.isRequired,
+  options: PropTypes.shape({
+    leading: PropTypes.bool,
+    trailing: PropTypes.bool,
+  }),
+  render: PropTypes.func,
+};
+
+TickingCounter.defaultProps = {
+  options: undefined,
+  render: val => val,
+};
+
+describe('WithThrottle', () => {
+  test('should render correctly with throttle', async (done) => {
+    const container = render(<TickingCounter
+      interval={100}
+      wait={250}
+    />);
+
+    const div = container.baseElement.querySelector('div');
+    expect(div.innerHTML).toBe('0');
+
+    await waitFor(110);
+    expect(div.innerHTML).toBe('0');
+
+    await waitFor(100);
+    expect(div.innerHTML).toBe('0');
+
+    await waitFor(100);
+    expect(div.innerHTML).toBe('2');
+
+    done();
+  });
+
+  test('should update throttle options', async (done) => {
+    const container = render(<TickingCounter
+      interval={100}
+      wait={250}
+    />);
+
+    const div = container.baseElement.querySelector('div');
+    expect(div.innerHTML).toBe('0');
+
+    container.rerender(<TickingCounter
+      interval={100}
+      wait={350}
+      options={{ trailing: true }}
+    />);
+
+    await waitFor(310);
+    // At this time, throttle has been renewed and invoked
+    // And update can be called again after 100 - 310 + 350 = 140
+    expect(div.innerHTML).toBe('1');
+
+    await waitFor(150);
+    expect(div.innerHTML).toBe('4');
+
+    done();
+  });
+
+  test('should rerender if children render prop changes', async (done) => {
+    const container = render(<TickingCounter
+      interval={100}
+      wait={250}
+    />);
+
+    const div = container.baseElement.querySelector('div');
+    expect(div.innerHTML).toBe('0');
+
+    await waitFor(100);
+
+    container.rerender(<TickingCounter
+      interval={100}
+      wait={250}
+      render={val => `foo-${val}`}
+    />);
+
+    expect(div.innerHTML).toBe('foo-0');
+
+    done();
+  });
+});
---
+++ __test__/jest.conf.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../'),
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+  moduleNameMapper: {
+    '^react-with-throttle': '<rootDir>/src',
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  coverageDirectory: '<rootDir>/__test__/coverage',
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/node_modules/**'],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
