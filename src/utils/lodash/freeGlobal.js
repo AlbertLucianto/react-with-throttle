336a1ba19dfe136f9b4df6e98fb404886c589153
AlbertLucianto <albertlucianto@gmail.com>
2019-03-07 21:45:06 +0800

    [Lodash] fork and use the (real) latest version of lodash

---
+++ src/utils/lodash/freeGlobal.js
@@ -0,0 +1,5 @@
+// eslint-disable
+/** Detect free variable `global` from Node.js. */
+const freeGlobal = typeof global === 'object' && global !== null && global.Object === Object && global;
+
+export default freeGlobal;
