4c54e574cb898ed1511ddb954b34b8831c53ce84
AlbertLucianto <albertlucianto@gmail.com>
2019-03-11 19:01:56 +0800

    [JSDoc] strip comments

--- src/utils/lodash/freeGlobal.js
+++ src/utils/lodash/freeGlobal.js
@@ -1,5 +1,3 @@
-// eslint-disable
-/** Detect free variable `global` from Node.js. */
 const freeGlobal = typeof global === 'object' && global !== null && global.Object === Object && global;
 
 export default freeGlobal;
--- src/utils/lodash/root.js
+++ src/utils/lodash/root.js
@@ -1,10 +1,8 @@
 /* eslint-disable */
 import freeGlobal from './freeGlobal';
 
-/** Detect free variable `self`. */
 const freeSelf = typeof self === 'object' && self !== null && self.Object === Object && self;
 
-/** Used as a reference to the global object. */
 const root = freeGlobal || freeSelf || Function('return this')();
 
 export default root;
